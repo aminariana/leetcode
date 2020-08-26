5ad0d0b6f175d298117a281234015e34c5c53b38
Amin Ariana <aminka@gmail.com>
2020-08-26 16:31:42 -0700

    Add a few corner cases

--- TrappingRainWater/SolutionTest.java
+++ TrappingRainWater/SolutionTest.java
@@ -6,7 +6,22 @@ import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
     @Test
-    void example() {
+    void empty() {
+        assertEquals(0, Solution.trap(new int[] { }));
+    }
+
+    @Test
+    void hill() {
+        assertEquals(0, Solution.trap(new int[] { 0, 1, 2, 1, 0 }));
+    }
+
+    @Test
+    void bowl() {
+        assertEquals(4, Solution.trap(new int[] { 2, 1, 0, 1, 2 }));
+    }
+
+    @Test
+    void normal() {
         assertEquals(6, Solution.trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
     }
 }
