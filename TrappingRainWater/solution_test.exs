91ed22bc572b80569b1673553b15aae293a18e4b
Amin Ariana <aminka@gmail.com>
2020-08-27 17:06:00 -0700

    Trapping Rain Water in Elixir

---
+++ TrappingRainWater/solution_test.exs
@@ -0,0 +1,23 @@
+Code.require_file("TrappingRainWater/solution.exs")
+
+ExUnit.start
+
+defmodule TrappingRainWaterTest do
+  use ExUnit.Case
+
+  test "empty" do
+    assert 0 == TrappingRainWater.trap([])
+  end
+
+  test "hill" do
+    assert 0 == TrappingRainWater.trap([0, 1, 2, 1, 0])
+  end
+
+  test "bowl" do
+    assert 4 == TrappingRainWater.trap([2, 1, 0, 1, 2])
+  end
+
+  test "normal" do
+    assert 6 == TrappingRainWater.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+  end
+end
