d76eecdb95b11684a91818f49694260fb9e9a2fa
Amin Ariana <aminka@gmail.com>
2024-02-22 17:27:52 -0800

    House Robber tests

--- HouseRobber/python/house_robber.py
+++ HouseRobber/python/house_robber.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def rob(self, nums: List[int]) -> int:
+def rob(nums: List[int]) -> int:
     # Rationale: At house i, either rob it and the best of up to two houses ago, or
     # the best of one house ago.
     last, best = 0, 0
--- HouseRobber/python/house_robber_test.py
+++ HouseRobber/python/house_robber_test.py
@@ -1,20 +1,27 @@
-# [1,2,3,1]
-# 4
+import pytest
+from HouseRobber.python import house_robber
 
-# [2,7,9,3,1]
-# 12
+class TestHouseRobber:
+    def test_empty(self):
+        assert 0 == house_robber.rob([])
 
-# [2]
-# 2
+    def test_zero(self):
+        assert 0 == house_robber.rob([0])
 
-# [0]
-# 0
+    def test_non_zero(self):
+        assert 2 == house_robber.rob([2])
 
-# [400, 2, 1]
-# 401
+    def test_example1(self):
+        assert 4 == house_robber.rob([1,2,3,1])
 
-# [3, 1, 3, 1, 3, 7]
-# 13
+    def test_example2(self):
+        assert 12 == house_robber.rob([2,7,9,3,1])
 
-# [3, 1, 3, 1, 3, 7, 0, 1]
-# 14
+    def test_rich(self):
+        assert 401 == house_robber.rob([400, 2, 1])
+
+    def test_ambiguous(self):
+        assert 13 == house_robber.rob([3, 1, 3, 1, 3, 7])
+
+    def test_unambiguous(self):
+        assert 14 == house_robber.rob([3, 1, 3, 1, 3, 7, 0, 1])
