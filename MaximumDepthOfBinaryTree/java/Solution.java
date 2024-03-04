82ea48842625ffc93f522f5aeda0566d1c89868e
Amin Ariana <aminka@gmail.com>
2024-03-04 13:35:38 -0800

    Max Tree Depth in Java

---
+++ MaximumDepthOfBinaryTree/java/Solution.java
@@ -0,0 +1,10 @@
+package MaximumDepthOfBinaryTree.java;
+
+import Common.java.TreeNode;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return (root == null)
+            ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
